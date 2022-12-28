23262ce19e50448d9c9207d175bfc8fab71eddb9
wee-owl <kem-cv@yandex.ru>
2022-12-28 16:47:15 +0300

    add solution

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
+"use strict"
+
+// task 3-1
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false
+  } else {
+    return arr1.every((item, i) => item === arr2[i])
+  }
 }
 
+
+// task 3-2
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let genderArray = users.filter((item) => item.gender === gender);
+  return genderArray.length ? genderArray.reduce((acc, item) => acc + item.age, 0)/genderArray.length : 0;
 }
