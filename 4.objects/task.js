63930dac010057878ff9698320df7bf914c665cf
wee-owl <kem-cv@yandex.ru>
2023-01-09 16:38:12 +0300

    add solution

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,24 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  return this.marks ? this.marks.push(...marks) : 'student expelled';
 }
 
 Student.prototype.getAverage = function () {
-  
+  return !this.marks || !this.marks.length ? 0 : this.marks.reduce((counter, value) => counter + value, 0)/this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
