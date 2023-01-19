5bc97a5dea98e46d970fca8c968345ed9949574c
wee-owl <kem-cv@yandex.ru>
2023-01-19 12:48:51 +0300

    add solution

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
-﻿
+﻿"use strict"
+
+// task 6-1
+function parseCount(value) {
+  if (isNaN(Number.parseFloat(value))) {
+    throw new Error("Невалидное значение");
+  }
+  return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+  parseCount(value);
+  try {
+    return parseCount(value);
+  } catch (error) {
+    throw new Error("Невалидное значение");
+  }
+}
+
+
+// task 6-2
+class Triangle {
+  constructor(sideA, sideB, sideC) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+
+    if (sideA+sideB<sideC || sideA+sideC<sideB || sideB+sideC<sideA) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+    return this.sideA + this.sideB + this.sideC;
+  }
+
+  get area() {
+    return +Math.sqrt((this.perimeter/2 - this.sideA)*
+    (this.perimeter/2 - this.sideB)*
+    (this.perimeter/2 - this.sideC)*
+    (this.perimeter/2)).toFixed(3);
+  }
+}
+
+function getTriangle(sideA, sideB, sideC) {
+  try {
+    return new Triangle(sideA, sideB, sideC);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует"
+      }, 
+      get area() {
+        return "Ошибка! Треугольник не существует"
+      }
+    };
+  }
+}
