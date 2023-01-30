03026ba69d2d2026e5e449d5bd6991be2813ca3b
wee-owl <kem-cv@yandex.ru>
2023-01-30 16:29:27 +0300

    delete logs in task 8-1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,24 +3,18 @@
 // task 8-1
 function cachingDecoratorNew(func) {
   let cache = [];
-
   function wrapper(...args) {
-    const hash = md5(args); // получаем правильный хеш c помощью функции md5
-      console.log(hash);
-      console.log(cache);
-    let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
-      console.log(objectInCache);
-    if (objectInCache) { // если элемент найден
-      console.log("Из кэша: " + objectInCache.result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
       return "Из кэша: " + objectInCache.result;
     }
 
-    let result = func(...args); // в кеше результата нет — придётся считать
-    cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+    let result = func(...args);
+    cache.push({hash, result});
     if (cache.length > 5) { 
-      cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+      cache.shift();
     }
-    console.log("Вычисляем: " + result);
     return "Вычисляем: " + result;  
   }
   return wrapper;
