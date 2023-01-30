4c6b8df538e061b948451d97e79b8b3f3e3ec078
wee-owl <kem-cv@yandex.ru>
2023-01-30 15:25:26 +0300

    test run task 8-1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,32 @@
-//Задача № 1
+'use strict'
+
+// task 8-1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args); // получаем правильный хеш c помощью функции md5
+      console.log(hash);
+      console.log(cache);
+    let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+      console.log(objectInCache);
+    if (objectInCache) { // если элемент найден
+      console.log("Из кэша: " + objectInCache.result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func(...args); // в кеше результата нет — придётся считать
+    cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+    if (cache.length > 5) { 
+      cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
-//Задача № 2
+// task 8-2
 function debounceDecoratorNew(func, delay) {
   
 }
